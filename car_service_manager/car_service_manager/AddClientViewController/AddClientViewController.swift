1ad6f946b9eac6f8bee483c89ed52c6a256f96a3
Gaev Anton <atoxf1@gmail.com>
2020-01-18 00:49:08 +0300

    added update model abolity

--- car_service_manager/car_service_manager/AddClientViewController/AddClientViewController.swift
+++ car_service_manager/car_service_manager/AddClientViewController/AddClientViewController.swift
@@ -22,7 +22,27 @@ class AddClientViewController: UIViewController {
     }
     
     @IBAction func save(_ sender: Any) {
-        ClientsFunctions.createClient(clientModel: ClientModel(clientName: clientTextField.text!))
+        
+        clientTextField.rightViewMode = .never
+        
+        guard clientTextField.text != "", let newClientName = clientTextField.text else {
+//            let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 30, height: 20))
+//            imageView.image = UIImage(systemName: "briefcase.fill")
+//            imageView.contentMode = .scaleAspectFit
+//            clientTextField.rightView = imageView
+            
+            clientTextField.layer.borderColor = UIColor.red.cgColor
+            clientTextField.layer.borderWidth = 1
+            clientTextField.layer.cornerRadius = 5
+            
+            clientTextField.placeholder = "Необходимо заполнить поле"
+            
+            clientTextField.rightViewMode = .always
+            
+            return
+        }
+        
+        ClientsFunctions.createClient(clientModel: ClientModel(clientName: newClientName))
         
         if let doneSaving = doneSaving {
             doneSaving()
